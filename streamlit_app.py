a7d76676b0ed3a97838dc2c317767bb7d7ab6a45
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 01:30:42 +0200

    Update streamlit_app.py
    Trying to establish snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.title('DHB Kader der Kontrahenten')
 streamlit.header('Füchse Berlin')
 streamlit.text('Hier platzieren wir die Kaderliste der Füchse Berlin.')
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
