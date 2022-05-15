0c7ee3eb2bb11b44bce5255b6b181bd0a34eca6d
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 01:54:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,8 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+my_cur.execute("select * from kader_berlin")
+my_data_rows = my_cur.fetchall()
+streamlit.header("Kaderliste Füchse Berlin")
+streamlit.dataframe(my_data_rows)
