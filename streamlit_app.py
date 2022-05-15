c003aa482ba4ecae459dbd0cf6f2dfe30bed367b
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 01:17:05 +0200

    Update streamlit_app.py
    nichts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('DHB Kader der Kontrahenten')
 streamlit.header('Füchse Berlin')
 streamlit.text('Hier platzieren wir die Kaderliste der Füchse Berlin.')
+
