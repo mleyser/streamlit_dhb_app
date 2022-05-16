7c171b8a6d1ab686603658d7ccd26983d2870688
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 08:33:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.text(my_data_row)
 
 my_cur.execute("SELECT * FROM kader_berlin")
 my_data_rows = my_cur.fetchall()
-data_berlin = st.table(my_data_rows)
+data_berlin = st.table(pd.DataFrame(my_data_rows, columns = ["Nachname","Vorname","Position","Spiele","Tore","Fehlwürfe","Feldtore","7-Meter-Tore","Wurfquote","Assists","Technische Fehler","Steals","Blocks","Gelbe Karten","2 min Strafen","Rote Karten", "Blaue Karten"]))
 
 
 
