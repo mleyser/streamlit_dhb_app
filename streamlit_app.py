dee49c311d06ee56e84ee7e98573352a4d776328
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 08:02:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,9 @@ my_data_row = my_cur.fetchone()
 st.text("Hello from Snowflake:")
 st.text(my_data_row)
 
+my_cur.execute("SELECT * FROM kader_berlin")
+my_data_rows = my_cur.fetchall()
+streamlit.header("Kader Füchse Berlin der Saison 2021/22")
+streamlit.dataframe(my_data_rows)
 
 
