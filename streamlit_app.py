9e5e7fc8a7f1a9f4e20b358ba7e26b38bd95f682
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 02:08:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,6 @@ st.text(my_data_row)
 my_cur.execute("select * from kader_berlin")
 my_data_rows = my_cur.fetchall()
 st.header("Kaderliste Füchse Berlin")
-st.dataframe(my_data_rows)
+df_berlin = pd.DataFrame(my_data_rows, columns = ['Nachname','Vorname','Position','Spiele','Tore','Fehlwürfe','Feldtore','7-Meter-Tore','Wurfquote','Assists','Technische Fehler','Steals','Blocks','Gelbe Karten','2 min Strafen','Rote Karten', 'Blaue Karten'])
+
+st.dataframe(df)
