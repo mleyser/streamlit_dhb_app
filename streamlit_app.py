c39d25bd5fde2ec02c1a37eaaf613ae176251851
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 09:02:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,7 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 
-
-
-st.title('DHB Kader der Kontrahenten')
-st.header('Füchse Berlin')
-st.text('Hier platzieren wir die Kaderliste der Füchse Berlin.')
-
+st.text("Establishing Snowflake connection")
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -15,8 +10,12 @@ my_data_row = my_cur.fetchone()
 st.text("Hello from Snowflake:")
 st.text(my_data_row)
 
+st.title('Füchse Berlin | Saison 2021/22')
+st.header("Spieler Gesamtstatistik")
+
+
 st.text("Spaltennamen | 0: Nachname, 1: Vorname, 2: POS, 3: S, 4: T, 5: FW, 6: FT, 7: 7M")
-st.text("\t\t\t\t|8: %, 9: AS, 10: TF, 11: ST, 12: BL, 13: GK, 14: 2M, 15: RK, 16: BK")
+st.text("8: %, 9: AS, 10: TF, 11: ST, 12: BL, 13: GK, 14: 2M, 15: RK, 16: BK")
 my_cur.execute("SELECT * FROM kader_berlin")
 my_data_rows = my_cur.fetchall()
 data_berlin = st.dataframe(my_data_rows)
