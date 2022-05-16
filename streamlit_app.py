50ff04b73a5f6c6e4f0aeeefb7f811883380e665
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 02:06:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,22 @@
-import streamlit
+import streamlit as st
 import snowflake.connector
 from urllib.error import URLError
 import pandas as pd
 import requests
 
 
-streamlit.title('DHB Kader der Kontrahenten')
-streamlit.header('Füchse Berlin')
-streamlit.text('Hier platzieren wir die Kaderliste der Füchse Berlin.')
+st.title('DHB Kader der Kontrahenten')
+st.header('Füchse Berlin')
+st.text('Hier platzieren wir die Kaderliste der Füchse Berlin.')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
 
 my_cur.execute("select * from kader_berlin")
 my_data_rows = my_cur.fetchall()
-streamlit.header("Kaderliste Füchse Berlin")
-streamlit.dataframe(my_data_rows)
+st.header("Kaderliste Füchse Berlin")
+st.dataframe(my_data_rows)
