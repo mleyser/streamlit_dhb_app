faf1d00200aa11a5395669d1cd42744049242054
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 23:04:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import snowflake.connector
 import pandas as pd
-import plotly.express as px
 from PIL import Image
 
 st.set_page_config(page_title = "Füchse Berlin", layout = "wide")
