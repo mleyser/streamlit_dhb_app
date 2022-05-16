c297344208f0408b4a4bd2f00ea3f160da5271b8
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-16 08:25:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import snowflake.connector
 import pandas as pd
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+
 
 
 st.title('DHB Kader der Kontrahenten')
@@ -19,18 +19,7 @@ my_cur.execute("SELECT * FROM kader_berlin")
 my_data_rows = my_cur.fetchall()
 data_berlin = st.table(my_data_rows)
 
-grid_response = AgGrid(
-    data_berlin,
-    gridOptions=gridOptions,
-    data_return_mode='AS_INPUT', 
-    update_mode='MODEL_CHANGED', 
-    fit_columns_on_grid_load=False,
-    theme='blue', #Add theme color to the table
-    enable_enterprise_modules=True,
-    height=350, 
-    width='100%',
-    reload_data=True
-)
+
 
 
 
